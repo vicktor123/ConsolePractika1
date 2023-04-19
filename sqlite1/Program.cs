dca814524bc350853ac6b2ea6c312f59b88fc9c6
user <user@H-D9F58A997E>
2023-04-19 14:59:54 +0300

    hello

---
+++ sqlite1/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data.SQLite;
+
+Console.WriteLine("Hello, sqlite!!!");
+
+using (var conn=new SQLiteConnection("Data Source=mydb.db;Version=3;New=True;Compress=True;"))
+{
+    conn.Open();
+    ReadData(conn);
+}
+static void ReadData(SQLiteConnection conn)
+{
+    
+
+    SQLiteCommand command = new SQLiteCommand("SELECT * FROM SampleTable", conn);
+
+    try
+    {
+        using (SQLiteDataReader reader = command.ExecuteReader())
+        {
+            if (reader.HasRows) // если есть данные
+            {
+                while (reader.Read())   // построчно считываем данные
+                {
+                    var id = reader.GetValue(0);
+                    var name = reader.GetValue(1);
+
+
+                    Console.WriteLine($"{id} \t {name} ");
+                }
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+
+
+    
+    conn.Close();
+}
